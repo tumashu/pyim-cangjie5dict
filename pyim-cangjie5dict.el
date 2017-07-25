a6730d351011c580591f27dda01c27d567de87b7
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-07-25 12:29:31 +0800

    * fix from wbdict

--- pyim-cangjie5dict.el
+++ pyim-cangjie5dict.el
@@ -29,19 +29,18 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
-;; * pyim-wbdict README                         :README:doc:
+;; * pyim-cangjie5dict README                         :README:doc:
 
 ;; ** 简介
 ;; pyim-cangjie5dict 是 pyim 的一个倉頡五代词库，修改自 RIME 项目。源于《五倉世紀》。
 
 ;; ** 安装和使用
 ;; 1. 配置melpa源，参考：http://melpa.org/#/getting-started
-;; 2. M-x package-install RET pyim-wbdict RET
+;; 2. M-x package-install RET pyim-cangjie5dict RET
 ;; 3. 在emacs配置文件中（比如: ~/.emacs）添加如下代码：
 ;;    #+BEGIN_EXAMPLE
-;;    (require 'pyim-wbdict)
-;;    (pyim-wbdict-gb2312-enable) ; gb2312 version
-;;    ;; (pyim-wbdict-gbk-enable) ; gbk version
+;;    (require 'pyim-cangjie5dict)
+;;    (pyim-cangjie5dict-enable)
 ;;    #+END_EXAMPLE
 
 ;;; Code:
@@ -64,4 +63,4 @@
 
 (provide 'pyim-cangjie5dict)
 
-;;; pyim-wbdict.el ends here
+;;; pyim-cangjie5dict.el ends here
